4fc7264f2f73551a380a9dd891c9a0fe78d5719b
Danny <danniharany@gmail.com>
2024-11-09 12:18:08 -0500

    resolver complete?

--- src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
+++ src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
@@ -4,5 +4,20 @@ import org.springframework.stereotype.Controller
 
 @Controller
 class EmployeesResolver {
+    // newEmployee, seems to be the only function called from client
+    @MutationMapping
+    fun newEmployee(@Argument("createEmployeeInput") input: createEmployeeInput): Employee {
+        if (input.name == null || input.dateOfBirth == null || input.city == null || input.salary == null) {
+            throw Exception("Missing required fields")
+        }
 
+        val employee = Employee(name = input.name,
+                                dateOfBirth = input.dateOfBirth,
+                                city = input.city,
+                                salary = input.salary)
+        
+        employee.UUID.randomUUID().toString()
+        employeeRepository.save(employee)
+        return employee;
+    }
 }
