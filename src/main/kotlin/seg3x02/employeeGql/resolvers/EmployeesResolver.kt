b2ffcf5b9c7e25f4e7baa9cf66011592ada066ef
Danny <danniharany@gmail.com>
2024-11-09 12:40:40 -0500

    resolver complete

--- src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
+++ src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
@@ -1,12 +1,18 @@
 package seg3x02.employeeGql.resolvers
 
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.stereotype.Controller
+import seg3x02.employeeGql.entity.Employee
+import seg3x02.employeeGql.repository.EmployeesRepository
+import seg3x02.employeeGql.resolvers.types.CreateEmployeeInput
+import java.util.*
 
 @Controller
-class EmployeesResolver {
+class EmployeesResolver(private val employeesRepository: EmployeesRepository) {
     // newEmployee, seems to be the only function called from client
     @MutationMapping
-    fun newEmployee(@Argument("createEmployeeInput") input: createEmployeeInput): Employee {
+    fun newEmployee(@Argument("createEmployeeInput") input: CreateEmployeeInput): Employee {
         if (input.name == null || input.dateOfBirth == null || input.city == null || input.salary == null) {
             throw Exception("Missing required fields")
         }
@@ -14,10 +20,12 @@ class EmployeesResolver {
         val employee = Employee(name = input.name,
                                 dateOfBirth = input.dateOfBirth,
                                 city = input.city,
+                                gender = input.gender,
+                                email = input.email,
                                 salary = input.salary)
         
-        employee.UUID.randomUUID().toString()
-        employeeRepository.save(employee)
+        employee.id = UUID.randomUUID().toString()
+        employeesRepository.save(employee)
         return employee;
     }
 }
